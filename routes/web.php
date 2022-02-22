4470e636a4d84f7635794e33d0aeab0b2f4df30d
neil <neil.yang0128@gmail.com>
2022-02-22 16:47:09 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function() {
-    return 'world';
-});
 
-Route::get('/hi', [App\Http\Controllers\Front\UserController::class, 'index']);
-
-Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    //
-    return $postId . $commentId;
-});
-
-Route::get('/messages', 'App\Http\Controllers\Front\MessageController@index');
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
